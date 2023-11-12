aa3b44419e15531f6e8a9f302b96bb58a7340bc9
ZachDaChampion <zachchampion79@gmail.com>
2023-11-11 20:40:26 -0500

    Support two cobots at once

--- launch/moveit_rviz.launch.py
+++ launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ar3", package_name="ar3_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("two_cobots", package_name="ar3_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
--- launch/static_virtual_joint_tfs.launch.py
+++ launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ar3", package_name="ar3_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("two_cobots", package_name="ar3_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
