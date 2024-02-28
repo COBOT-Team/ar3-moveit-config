a66568f7d6696ef8b63f3e2708e56d087a5e11bc
cobot-pc <n/a>
2024-02-28 16:46:32 -0500

    Multiple configs

--- single-cobot-moveit-config/launch/spawn_controllers.launch.py
+++ single-cobot-moveit-config/launch/spawn_controllers.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("two_cobots", package_name="ar3_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("cobot0", package_name="single_cobot_moveit_config").to_moveit_configs()
     return generate_spawn_controllers_launch(moveit_config)
--- single-cobot-moveit-config/launch/warehouse_db.launch.py
+++ single-cobot-moveit-config/launch/warehouse_db.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("two_cobots", package_name="ar3_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("cobot0", package_name="single_cobot_moveit_config").to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
